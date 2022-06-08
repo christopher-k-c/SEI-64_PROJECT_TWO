0a04f954e7a62865a3b37f36de5ebfe3d912e059
Yamilyn <yamilysousa@hotmail.com>
2022-06-08 15:51:17 +0100

    dashboard created

--- public/script/main.js
+++ public/script/main.js
@@ -1,9 +1,41 @@
 $( document ).ready(function() {
+    
     $(".delete").click(function() {
         if (confirm("Are you sure? This cannot be undone.")) {
             return true
         }
         return false
     });
+
+// Script nav bar
+
+    // window.addEventListener('DOMContentLoaded', event => {
+
+        // Toggle the side navigation
+        const sidebarToggle = document.body.querySelector('#sidebarToggle');
+        if (sidebarToggle) {
+            // Uncomment Below to persist sidebar toggle between refreshes
+            // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+            //     document.body.classList.toggle('sb-sidenav-toggled');
+            // }
+            sidebarToggle.addEventListener('click', event => {
+                event.preventDefault();
+                document.body.classList.toggle('sb-sidenav-toggled');
+                localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+            });
+        }
+    // });
+
+// Data tables
+
+    // window.addEventListener('DOMContentLoaded', event => {
+        // Simple-DataTables
+        // https://github.com/fiduswriter/Simple-DataTables/wiki
+    
+        const datatablesSimple = document.getElementById('datatablesSimple');
+        if (datatablesSimple) {
+            new simpleDatatables.DataTable(datatablesSimple);
+        }
+    // });
 });
 
